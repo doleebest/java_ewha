58450a9be0cb6927fab30a26fb9bb9e8de7894bc
doleebest <90204371+doleebest@users.noreply.github.com>
2022-03-22 20:11:54 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,23 @@
+
+public class Hello {
+	
+	public static int sum(int n, int m) { // µ¡¼À ÇÔ¼ö
+		return n+m;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 20;
+		int s;
+		char a;
+		
+		s = sum(i, 10); // s = 30
+		a = '?';
+		System.out.println(a); // ?
+		System.out.println("Hello"); // Hello
+		System.out.println(s); // 30
+		
+		
+	}
+
+}
