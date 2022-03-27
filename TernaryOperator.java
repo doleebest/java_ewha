1c728e1734a8daa65f86b0e82e12bd856e9e5835
doleebest <90204371+doleebest@users.noreply.github.com>
2022-03-28 00:23:36 +0900

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,11 @@
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3, b=5;
+		System.out.println("두수의 차이는 "+((a>b)?(a-b):(b-a)));
+		
+	}
+
+}
