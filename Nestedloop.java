21a67970db8f3895b87fc34b0e84bd535762d54f
doleebest <90204371+doleebest@users.noreply.github.com>
2022-04-04 01:52:59 +0900

    Add files via upload

---
+++ Nestedloop.java
@@ -0,0 +1,17 @@
+
+public class Nestedloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1; i<10; i++) {
+			for (int j=1; j<10; j++) {
+				System.out.print(i+"*"+j+"="+i*j); // 1*1=1
+				System.out.print('\t'); // ег
+			}
+		System.out.println();
+		}
+		
+	}
+
+}
